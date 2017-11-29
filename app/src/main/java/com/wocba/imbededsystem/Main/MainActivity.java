9165cc411d1512d9587ad71fee0ddffcf93be323
jinwo <jinwoo002@naver.com>
2017-11-30 02:16:54 +0900

    detailDialog 구현 및 데이터 이동 확인

--- app/src/main/java/com/wocba/imbededsystem/Main/MainActivity.java
+++ app/src/main/java/com/wocba/imbededsystem/Main/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback, Go
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present..
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
