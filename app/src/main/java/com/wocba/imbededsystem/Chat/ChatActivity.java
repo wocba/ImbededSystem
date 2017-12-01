4f367713345077b57bb42ae75f3cd77520611686
YoRHaGruppe <manga707@naver.com>
2017-12-01 17:51:00 +0900

    chat activity commit

---
+++ app/src/main/java/com/wocba/imbededsystem/Chat/ChatActivity.java
@@ -0,0 +1,126 @@
+package com.wocba.imbededsystem.Chat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.wocba.imbededsystem.R;
+
+/**
+ * Created by Administrator on 2017-11-29.
+ */
+
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
+    // Firebase
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    // Views
+    private ListView mListView;
+    private EditText mEdtMessage;
+    // Values
+    private ChatAdapter mAdapter;
+    private String userName;
+
+    private InputMethodManager imm;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        initViews();
+        initFirebaseDatabase();
+        initValues();
+    }
+
+    private void hideKeyboard(){
+        imm.hideSoftInputFromWindow(mEdtMessage.getWindowToken(),0);
+    }
+
+    private void initViews() {
+        mListView = (ListView) findViewById(R.id.list_message);
+        mAdapter = new ChatAdapter(this, 0);
+        mListView.setAdapter(mAdapter);
+
+        mEdtMessage = (EditText) findViewById(R.id.edit_message);
+        findViewById(R.id.btn_send).setOnClickListener(this);
+    }
+
+    private void initFirebaseDatabase() {
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference("message");
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                ChatData chatData = dataSnapshot.getValue(ChatData.class);
+                chatData.firebaseKey = dataSnapshot.getKey();
+                mAdapter.add(chatData);
+                mListView.smoothScrollToPosition(mAdapter.getCount());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String firebaseKey = dataSnapshot.getKey();
+                int count = mAdapter.getCount();
+                for (int i = 0; i < count; i++) {
+                    if (mAdapter.getItem(i).firebaseKey.equals(firebaseKey)) {
+                        mAdapter.remove(mAdapter.getItem(i));
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildEventListener);
+    }
+
+    private void initValues() {
+        Intent intent = getIntent();
+        userName = intent.getStringExtra("nickname");
+        //userName = "Guest" + new Random().nextInt(5000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDatabaseReference.removeEventListener(mChildEventListener);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String message = mEdtMessage.getText().toString();
+        if (!TextUtils.isEmpty(message)) {
+            mEdtMessage.setText("");
+            ChatData chatData = new ChatData();
+            chatData.userName = userName;
+            chatData.message = message;
+            chatData.time = System.currentTimeMillis();
+            mDatabaseReference.push().setValue(chatData);
+        }
+    }
+}
+
