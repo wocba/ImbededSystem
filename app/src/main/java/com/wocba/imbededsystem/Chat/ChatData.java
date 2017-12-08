ec5fb05c7f8f6ee87f960fc7f068807a5c6638a5
YoRHaGruppe <manga707@naver.com>
2017-12-08 21:58:47 +0900

    채팅창 변경 및 타이핑 해결

--- app/src/main/java/com/wocba/imbededsystem/Chat/ChatData.java
+++ app/src/main/java/com/wocba/imbededsystem/Chat/ChatData.java
@@ -9,7 +9,7 @@ public class ChatData {
     public String senderEmail; // 메세지를 보낸 사용자의 이메일주소
     public String message; // 작성한 메시지 내용
     public String time; // 메세지를 작성한 시간
-    public String senderNickname; // 메세지를 보낸 사용자의 닉네임
+    //public String senderNickname; // 메세지를 보낸 사용자의 닉네임
     public String senderPhotoUrl; // 메세지를 보낸 사용자의 사진 URL
 
 }
